7b1eaa48936a0c071283a90a77cd25a5742b51ea
ivan chu <ivan.csq@hotmail.com>
2024-05-23 16:22:32 +0800

    function all work

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,18 @@
 class TasksController < ApplicationController
+before_action :set_task_id, only:[:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
   end
 
   def new
     # create blank instance
     @task = Task.new
+    # raise
   end
 
   def create
@@ -19,11 +22,31 @@ class TasksController < ApplicationController
       redirect_to task_path(task)
     end
   end
-end
 
-private
+  def edit
+    # @task = Task.find(params[:id])
+    # raise
+  end
 
-def task_params
-  raise
-  params.require(:task).permit(:title, :details)
+  def update
+    # @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    # @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task_id
+    @task = Task.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,13 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  get 'tasks', to: 'tasks#index' # GET /tasks
-  get 'tasks/new', to: 'tasks#new' # GET /tasks/new
-  get 'tasks/:id', to: 'tasks#show', as: 'task' # GET /tasks/1
-  post 'tasks', to: 'tasks#create' # POST /tasks
+  # get 'tasks', to: 'tasks#index' # GET /tasks
+  # get 'tasks/new', to: 'tasks#new' # GET /tasks/new
+  # get 'tasks/:id', to: 'tasks#show', as: 'task' # GET /tasks/1
+  # post 'tasks', to: 'tasks#create' # POST /tasks
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: 'task_edit' # GET /tasks/id/edit
+  # patch 'tasks/:id', to: 'tasks#update'
+  # delete 'tasks/:id', to: 'tasks#destroy', as: 'task_delete'
+
+  resources :tasks
 end
