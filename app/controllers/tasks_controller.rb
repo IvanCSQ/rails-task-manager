34a33421b8ba662cfb3feb8f8406b2e49c458e14
ivan chu <ivan.csq@hotmail.com>
2024-05-23 13:59:26 +0800

    complete till step 5

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,28 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    # create blank instance
+    @task = Task.new
+  end
+
+  def create
+    task = Task.new(task_params)
+
+    if task.save
+      redirect_to task_path(task)
+    end
+  end
+end
+
+private
+
+def task_params
+  raise
+  params.require(:task).permit(:title, :details)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  get 'tasks', to: 'tasks#index'
+  get 'tasks', to: 'tasks#index' # GET /tasks
+  get 'tasks/new', to: 'tasks#new' # GET /tasks/new
+  get 'tasks/:id', to: 'tasks#show', as: 'task' # GET /tasks/1
+  post 'tasks', to: 'tasks#create' # POST /tasks
 end
